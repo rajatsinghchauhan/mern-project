2020659fb81f353c18f3814cf0c820e5e4195224
rajatsinghchauhan <singhrajatmyworld@gmail.com>
2022-12-14 16:10:18 +0530

    updated review card

--- frontend/src/component/product/productDetails.js
+++ frontend/src/component/product/productDetails.js
@@ -1,30 +1,273 @@
-import React, { Fragment, useEffect, useState } from "react";
+// import React, { Fragment, useEffect, useState } from "react";
 
+// import Carousel from "react-material-ui-carousel";
+// import { useSelector, useDispatch } from "react-redux";
+// import "./productDetails.css";
+// import { clearErrors, getProductdetails } from "../../actions/productAction";
+// import ReactStars from "react-rating-stars-component";
+// import ReviewCard from "./reviewCard.js";
+// import Loading from "../layout/Loading/loading";
+// import { useAlert } from "react-alert";
+// import Metadata from "../layout/metadata";
+// import { addItemsToCart } from "../../actions/cartAction";
+// import { newReview } from "../../actions/productAction";
+// import {
+//   Dialog,
+//   DialogActions,
+//   DialogContent,
+//   DialogTitle,
+//   Button,
+// } from "@material-ui/core";
+
+// import { Rating } from "@material-ui/lab";
+// import { NEW_REVIEW_RESET } from "../../constants/productConstants";
+
+// const ProductDetails = ({ match }) => {
+//   const alert = useAlert();
+//   const dispatch = useDispatch();
+//   const { loading, product, error } = useSelector(
+//     (state) => state.productDetails
+//   );
+//   const { success, error: reviewError } = useSelector(
+//     (state) => state.newReview
+//   );
+
+//   const [quantity, setQuantity] = useState(1);
+//   const [open, setOpen] = useState(false);
+//   const [rating, setRating] = useState(0);
+//   const [comment, setComment] = useState("");
+
+//   const increaseQuantity = () => {
+//     if (product.stock <= quantity) return;
+//     const qty = quantity + 1;
+//     setQuantity(qty);
+//   };
+//   const decreaseQuantity = () => {
+//     if (1 >= quantity) return;
+
+//     const qty = quantity - 1;
+//     setQuantity(qty);
+//   };
+
+//   const addToCartHandler = () => {
+//     dispatch(addItemsToCart(match.params.id, quantity));
+//     alert.success("Item Added To Cart");
+//   };
+
+//   const submitReviewToggle = () => {
+//     open ? setOpen(false) : setOpen(true);
+//   };
+
+//   const reviewSubmitHandler = () => {
+//     const myForm = new FormData();
+
+//     myForm.set("rating", rating);
+//     myForm.set("comment", comment);
+//     myForm.set("productId", match.params.id);
+
+//     dispatch(newReview(myForm));
+
+//     setOpen(false);
+//   };
+
+//   useEffect(() => {
+//     if (error) {
+//       alert.error(error);
+//       dispatch(clearErrors());
+//     }
+
+//     if (reviewError) {
+//       alert.error(reviewError);
+//       dispatch(clearErrors());
+//     }
+
+//     if (success) {
+//       alert.success("Review Submitted Successfully");
+//       dispatch({ type: NEW_REVIEW_RESET });
+//     }
+//     dispatch(getProductdetails(match.params.id));
+//   }, [dispatch, match.params.id, error, alert, reviewError, success]);
+
+//   const options = {
+//     edit: false,
+//     color: "rgb(20,20,20,0.1)",
+//     activeColor: "tomato",
+//     size: window.innerHeight < 200 ? 20 : 25,
+//     value: Number(product.rating),
+//     isHalf: true,
+//   };
+
+//   return (
+//     <Fragment>
+//       {loading ? (
+//         <Loading />
+//       ) : (
+//         <Fragment>
+//           <Metadata title={`${product.name}`} />
+//           <div className="ProductDetails">
+//             <div>
+//               <Carousel>
+//                 {product.images &&
+//                   product.images.map((item, i) => (
+//                     <img
+//                       className="CarouselImage"
+//                       key={item.url}
+//                       src={item.url}
+//                       alt={`${i} Slide`}
+//                     />
+//                   ))}
+//               </Carousel>
+//             </div>
+//             <div>
+//               <div className="detailsBlock-1">
+//                 <h2>{product.name}</h2>
+//                 <p>Product # {product._id}</p>
+//               </div>
+//               <div className="detailsBlock-2">
+//                 <ReactStars {...options} />
+
+//                 <span className="detailsBlock-2-span">
+//                   ({product.numOfReviews} Reviews)
+//                 </span>
+//               </div>
+//               <div className="detailsBlock-3">
+//                 <h1>{`₹${product.price}`}</h1>
+//                 <div className="detailsBlock-3-1">
+//                   <div className="detailsBlock-3-1-1">
+//                     <button onClick={decreaseQuantity}>-</button>
+//                     <input readOnly value={quantity} type="number" />
+//                     <button onClick={increaseQuantity}>+</button>
+//                   </div>
+//                   <button
+//                     disabled={product.stock < 1 ? true : false}
+//                     onClick={addToCartHandler}
+//                   >
+//                     Add to Cart
+//                   </button>
+//                 </div>
+
+//                 <p>
+//                   Status:{""}
+//                   <b className={product.stock < 1 ? "redColor" : "greenColor"}>
+//                     {product.stock < 1 ? "OutOfStock" : "InStock"}
+//                   </b>
+//                 </p>
+//               </div>
+//               <div className="detailsBlock-4">
+//                 Description : <p>{product.description}</p>
+//               </div>
+//               <button onClick={submitReviewToggle} className="submitReview">
+//                 Submit Review
+//               </button>
+//             </div>
+//           </div>
+//           <h3 className="reviewsHeading">REVIEWS</h3>
+
+//           <Dialog
+//             aria-labelledby="simple-dialog-title"
+//             open={open}
+//             onClose={submitReviewToggle}
+//           >
+//             <DialogTitle>Submit Review</DialogTitle>
+//             <DialogContent className="submitDialog">
+//               <Rating
+//                 onChange={(e) => setRating(e.target.value)}
+//                 value={rating}
+//                 size="large"
+//               />
+
+//               <textarea
+//                 className="submitDialogTextArea"
+//                 cols="30"
+//                 rows="5"
+//                 value={comment}
+//                 onChange={(e) => setComment(e.target.value)}
+//               ></textarea>
+//             </DialogContent>
+//             <DialogActions>
+//               <Button onClick={submitReviewToggle} color="secondary">
+//                 Cancel
+//               </Button>
+//               <Button onClick={reviewSubmitHandler} color="primary">
+//                 Submit
+//               </Button>
+//             </DialogActions>
+//           </Dialog>
+
+//           {product.reviews && product.reviews[0] ? (
+//             <div className="reviews">
+//               {product.reviews &&
+//                 product.reviews.map((review) => (
+//                   <ReviewCard key={review._id} review={review} />
+//                 ))}
+//             </div>
+//           ) : (
+//             <p className="noReviews">No Reviews Yet</p>
+//           )}
+//         </Fragment>
+//       )}
+//     </Fragment>
+//   );
+// };
+
+// export default ProductDetails;
+
+import React, { Fragment, useEffect, useState } from "react";
 import Carousel from "react-material-ui-carousel";
-import { useSelector, useDispatch } from "react-redux";
 import "./productDetails.css";
-import { clearErrors, getProductdetails } from "../../actions/productAction";
-import ReactStars from "react-rating-stars-component";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  clearErrors,
+  getProductdetails,
+  newReview,
+} from "../../actions/productAction";
+
 import ReviewCard from "./reviewCard.js";
 import Loading from "../layout/Loading/loading";
 import { useAlert } from "react-alert";
-import Metadata from "../layout/metadata";
+import MetaData from "../layout/metadata";
 import { addItemsToCart } from "../../actions/cartAction";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Button,
+} from "@material-ui/core";
+import { Rating } from "@material-ui/lab";
+import { NEW_REVIEW_RESET } from "../../constants/productConstants";
 
 const ProductDetails = ({ match }) => {
-  const alert = useAlert();
   const dispatch = useDispatch();
-  const { loading, product, error } = useSelector(
+  const alert = useAlert();
+
+  const { product, loading, error } = useSelector(
     (state) => state.productDetails
   );
 
+  const { success, error: reviewError } = useSelector(
+    (state) => state.newReview
+  );
+
+  const options = {
+    size: "large",
+    value: product.rating,
+    readOnly: true,
+    precision: 0.5,
+  };
+
   const [quantity, setQuantity] = useState(1);
+  const [open, setOpen] = useState(false);
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState("");
 
   const increaseQuantity = () => {
     if (product.stock <= quantity) return;
+
     const qty = quantity + 1;
     setQuantity(qty);
   };
+
   const decreaseQuantity = () => {
     if (1 >= quantity) return;
 
@@ -37,25 +280,39 @@ const ProductDetails = ({ match }) => {
     alert.success("Item Added To Cart");
   };
 
-  useEffect(
-    (error) => {
-      if (error) {
-        alert.error(error);
-        dispatch(clearErrors());
-      }
-      dispatch(getProductdetails(match.params.id));
-    },
-    [dispatch, match.params.id, error, alert]
-  );
-
-  const options = {
-    edit: false,
-    color: "rgb(20,20,20,0.1)",
-    activeColor: "tomato",
-    size: window.innerHeight < 200 ? 20 : 25,
-    value: Number(product.rating),
-    isHalf: true,
+  const submitReviewToggle = () => {
+    open ? setOpen(false) : setOpen(true);
   };
+
+  const reviewSubmitHandler = () => {
+    const myForm = new FormData();
+
+    myForm.set("rating", rating);
+    myForm.set("comment", comment);
+    myForm.set("productId", match.params.id);
+
+    dispatch(newReview(myForm));
+
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
+
+    if (reviewError) {
+      alert.error(reviewError);
+      dispatch(clearErrors());
+    }
+
+    if (success) {
+      alert.success("Review Submitted Successfully");
+      dispatch({ type: NEW_REVIEW_RESET });
+    }
+    dispatch(getProductdetails(match.params.id));
+  }, [dispatch, match.params.id, error, alert, reviewError, success]);
 
   return (
     <Fragment>
@@ -63,7 +320,7 @@ const ProductDetails = ({ match }) => {
         <Loading />
       ) : (
         <Fragment>
-          <Metadata title={`${product.name}`} />
+          <MetaData title={`${product.name} -- ECOMMERCE`} />
           <div className="ProductDetails">
             <div>
               <Carousel>
@@ -71,23 +328,24 @@ const ProductDetails = ({ match }) => {
                   product.images.map((item, i) => (
                     <img
                       className="CarouselImage"
-                      key={item.url}
+                      key={i}
                       src={item.url}
                       alt={`${i} Slide`}
                     />
                   ))}
               </Carousel>
             </div>
+
             <div>
               <div className="detailsBlock-1">
                 <h2>{product.name}</h2>
                 <p>Product # {product._id}</p>
               </div>
               <div className="detailsBlock-2">
-                <ReactStars {...options} />
-
+                <Rating {...options} />
                 <span className="detailsBlock-2-span">
-                  ({product.numOfReviews} Reviews)
+                  {" "}
+                  ({product.numofreviews} Reviews)
                 </span>
               </div>
               <div className="detailsBlock-3">
@@ -95,26 +353,67 @@ const ProductDetails = ({ match }) => {
                 <div className="detailsBlock-3-1">
                   <div className="detailsBlock-3-1-1">
                     <button onClick={decreaseQuantity}>-</button>
-                    <input readOnly value={quantity} type="number" />
+                    <input readOnly type="number" value={quantity} />
                     <button onClick={increaseQuantity}>+</button>
                   </div>
-                  <button onClick={addToCartHandler}>Add to Cart</button>
+                  <button
+                    disabled={product.stock < 1 ? true : false}
+                    onClick={addToCartHandler}
+                  >
+                    Add to Cart
+                  </button>
                 </div>
 
                 <p>
-                  Status:{""}
-                  <b className={product.Stock < 1 ? "redColor" : "greenColor"}>
-                    {product.Stock < 1 ? "OutOfStock" : "InStock"}
+                  Status:
+                  <b className={product.stock < 1 ? "redColor" : "greenColor"}>
+                    {product.stock < 1 ? "OutOfStock" : "InStock"}
                   </b>
                 </p>
               </div>
+
               <div className="detailsBlock-4">
                 Description : <p>{product.description}</p>
               </div>
-              <button className="submitReview">Submit Review</button>
+
+              <button onClick={submitReviewToggle} className="submitReview">
+                Submit Review
+              </button>
             </div>
           </div>
+
           <h3 className="reviewsHeading">REVIEWS</h3>
+
+          <Dialog
+            aria-labelledby="simple-dialog-title"
+            open={open}
+            onClose={submitReviewToggle}
+          >
+            <DialogTitle>Submit Review</DialogTitle>
+            <DialogContent className="submitDialog">
+              <Rating
+                onChange={(e) => setRating(e.target.value)}
+                value={rating}
+                size="large"
+              />
+
+              <textarea
+                className="submitDialogTextArea"
+                cols="30"
+                rows="5"
+                value={comment}
+                onChange={(e) => setComment(e.target.value)}
+              ></textarea>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={submitReviewToggle} color="secondary">
+                Cancel
+              </Button>
+              <Button onClick={reviewSubmitHandler} color="primary">
+                Submit
+              </Button>
+            </DialogActions>
+          </Dialog>
 
           {product.reviews && product.reviews[0] ? (
             <div className="reviews">
--- frontend/src/component/product/reviewCard.js
+++ frontend/src/component/product/reviewCard.js
@@ -1,25 +1,47 @@
+// import React from "react";
+// import ReactStars from "react-rating-stars-component";
+// import profilePng from "../../images/Profile.png";
+// import "./productDetails.css";
+
+// const reviewCard = ({ review }) => {
+//   const options = {
+//     edit: false,
+//     color: "rgb(20,20,20,0.1)",
+//     activeColor: "tomato",
+//     size: window.innerHeight < 200 ? 20 : 25,
+//     value: Number(review.rating),
+//     isHalf: true,
+//   };
+//   return (
+//     <div className="reviewCard">
+//       <img src={profilePng} alt="" />
+//       <p>{review.name}</p>
+//       <ReactStars {...options} />
+//       <span className="reviewCardComment">{review.coment}</span>
+//     </div>
+//   );
+// };
+
+// export default reviewCard;
+
+import { Rating } from "@material-ui/lab";
 import React from "react";
-import ReactStars from "react-rating-stars-component";
 import profilePng from "../../images/Profile.png";
-import "./productDetails.css";
 
 const reviewCard = ({ review }) => {
   const options = {
-    edit: false,
-    color: "rgb(20,20,20,0.1)",
-    activeColor: "tomato",
-    size: window.innerHeight < 200 ? 20 : 25,
-    value: Number(review.rating),
-    isHalf: true,
+    value: review.rating,
+    readOnly: true,
+    precision: 0.5,
   };
+
   return (
     <div className="reviewCard">
-      <img src={profilePng} alt="" />
+      <img src={profilePng} alt="User" />
       <p>{review.name}</p>
-      <ReactStars {...options} />
-      <span className="reviewCardComment">{review.coment}</span>
+      <Rating {...options} />
+      <span className="reviewCardComment">{review.comment}</span>
     </div>
   );
 };
-
 export default reviewCard;
